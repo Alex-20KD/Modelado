d885f6eff0deeb3b5e7baec52b2d24eaea1f4c56
Alex-20KD <gonzaloyzam.2018@gmail.com>
2025-06-06 02:30:24 -0500

    modificando requirements

--- src/supabase_client.py
+++ src/supabase_client.py
@@ -1,5 +1,7 @@
-from supabase import create_client, Client
+import os
+from supabase import create_client
 
-url: str = "https://xjgxgdghvdsszygtftms.supabase.co"
-key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhqZ3hnZGdodmRzc3p5Z3RmdG1zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkxNjk0MjMsImV4cCI6MjA2NDc0NTQyM30.mXR-2OSCwXHqxqOiRT3iR9jRwDpo2ozKwAQaTWsej78"
-supabase: Client = create_client(url, key)
+url = os.getenv("SUPABASE_URL")
+key = os.getenv("SUPABASE_KEY")
+
+supabase = create_client(url, key)
