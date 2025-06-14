e8ae5f7e3dfb44315cf25619dc1c532e9fb988d4
Alex-20KD <gonzaloyzam.2018@gmail.com>
2025-06-13 21:54:51 -0500

    Subir

--- src/supabase_client.py
+++ src/supabase_client.py
@@ -1,7 +1,14 @@
 import os
 from supabase import create_client
+from dotenv import load_dotenv
+from pathlib import Path # 1. Importa la clase Path
+
+env_path = Path(__file__).parent.parent / '.env'
+
+load_dotenv(dotenv_path=env_path)
 
 url = os.getenv("SUPABASE_URL")
 key = os.getenv("SUPABASE_KEY")
 
 supabase = create_client(url, key)
+
